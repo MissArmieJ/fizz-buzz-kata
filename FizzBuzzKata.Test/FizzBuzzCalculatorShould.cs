32dcee6625ea3beb07d2310888f1e3d9d580f42d
Armanda <adp@armanda.me.uk>
2017-06-05 16:53:54 +0100

    first fizz buzz classicist test

---
+++ FizzBuzzKata.Test/FizzBuzzCalculatorShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.Test
+{
+    [TestFixture]
+    public class FizzBuzzCalculatorShould
+    {
+        [Test]
+        public void calculate_1_if_input_is_1()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            var output = fizzBuzz.Calculate(1);
+
+            Assert.That(output, Is.EqualTo("1"));
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public string Calculate(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
