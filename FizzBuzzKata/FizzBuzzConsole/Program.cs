89ac9511e41a30e42be6679a2dcb30ef9114d314
Armanda <adp@armanda.me.uk>
2017-06-05 17:37:26 +0100

    add console (no need to over engineer further)

---
+++ FizzBuzzKata/FizzBuzzConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FizzBuzzKata;
+
+namespace FizzBuzzConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(fizzBuzz.Calculate(i));                
+            }
+            Console.ReadLine();
+        }
+    }
+}
